7f9f4d999b4557337d313013044f08def52ca444
Austerlitz1203 <1477567612@qq.com>
2023-09-16 21:19:35 +0800

    通讯录3.0（使用未知字段）

--- update/client/client.cc
+++ update/client/client.cc
@@ -1,11 +1,14 @@
 #include<iostream>
 #include<fstream>
 #include<contacts.pb.h>
+#include<google/protobuf/unknown_field_set.h>
 
 using namespace std;
 using namespace contacts;
+using namespace google::protobuf;
 
-void PrintContacts(contacts::Contacts &cons)
+    void
+    PrintContacts(contacts::Contacts &cons)
 {
     int n=cons.contacts_size();
     for(int i=0;i<n;i++)
@@ -20,6 +23,33 @@ void PrintContacts(contacts::Contacts &cons)
             const ::contacts::Phone &pho = con.phone(j);
             cout<<"联系人电话"<< j+1 <<":"<<pho.phone_number()<<endl;
         }
+
+        const Reflection* reflection = PeopleInfo::GetReflection();
+        const UnknownFieldSet&  set = reflection->GetUnknownFields(con);
+        int n=set.field_count();
+        for(int i=0;i<n;i++){
+            const UnknownField &  unknowfield = set.field(i);
+            cout<<"未知字段"<<i+1<<":"
+                <<"字段编号："<<unknowfield.number();
+            switch (unknowfield.type())
+            {
+            case UnknownField::Type::TYPE_VARINT:
+                cout<< "  值："<<unknowfield.varint()<<endl;
+                break;
+            case UnknownField::Type::TYPE_FIXED32:
+                cout << "  值：" << unknowfield.fixed32() << endl;
+                break;
+            case UnknownField::Type::TYPE_FIXED64:
+                cout << "  值：" << unknowfield.fixed64() << endl;
+                break;
+            case UnknownField::Type::TYPE_LENGTH_DELIMITED:
+                cout << "  值：" << unknowfield.length_delimited() << endl;
+                break;
+            default:
+                break;
+            }
+        }
+
 
     }
 }
